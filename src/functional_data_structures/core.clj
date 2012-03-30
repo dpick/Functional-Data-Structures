bfa2a5c318e41edffd4a0b2f546614ef516f8101
David Pick <pickdavid@gmail.com>
2012-03-29 21:30:29 -0500

    adding suffixes method from chapter 2

--- src/functional_data_structures/core.clj
+++ src/functional_data_structures/core.clj
@@ -1,1 +1,12 @@
-(ns functional_data_structures.core)
+(ns functional_data_structures.core
+  (:gen-class))
+
+(defn suffixes
+  ([coll1] (suffixes coll1 []))
+  ([coll1 coll2]
+  (if (empty? coll1)
+    (conj coll2 coll1)
+    (suffixes (rest coll1) (conj coll2 coll1)))))
+
+(defn -main [& args]
+  (println (suffixes [1 2 3 4])))
