7c30cd9408b294576dc433af92bf16fba7546f14
David Pick <pickdavid@gmail.com>
2012-04-01 21:03:47 -0500

    adding a probably broken fast-member function

--- src/functional_data_structures/core.clj
+++ src/functional_data_structures/core.clj
@@ -10,13 +10,25 @@
     (suffixes (rest coll1) (conj coll2 coll1)))))
 
 ; from chap 2
-(defn member [elem tree]
+; this implementation of member takes 2 * depth comparisions
+(defn slow-member [elem tree]
   (let [root (second tree) left (first tree) right (last tree)]
     (cond
       (empty? tree) false
-      (< elem root) (member elem left)
-      (> elem root) (member elem right)
+      (< elem root) (slow-member elem left)
+      (> elem root) (slow-member elem right)
       :else true)))
 
+; problem 2.3
+; this implementation of member takes depth + 1 comparisions
+(defn fast-member
+  ([elem tree] (fast-member elem tree nil))
+  ([elem tree prev]
+   (let [root (second tree) left (first tree) right (last tree)]
+     (cond
+       (and (nil? left) (nil? right)) (or (= root elem) (= prev elem))
+       (< elem root) (fast-member elem left root)
+       :else (fast-member elem right root)))))
+
 (defn -main [& args]
-  (println (member 3 [[1 3 4] 7 5])))
+  (println (fast-member 5 [[nil 2 nil] 3 [nil 4 nil]])))
